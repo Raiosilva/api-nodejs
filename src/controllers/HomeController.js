6b212bae1280705d6ff968e364c39aff4b48bbc5
Raiosilva <raimundopi025@gmail.com>
2020-05-17 20:22:45 -0300

    router alunos e validação email unico

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Raimundo',
-      sobrenome: 'Oliveira',
-      email: 'raimundo@gmail.com',
-      idade: 40,
-      peso: 23,
-      altura: 1.7,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
