4412ea523a8f467b296ce63648ef724e5870a2f5
Raiosilva <raimundopi025@gmail.com>
2020-05-19 12:25:11 -0300

    ajustes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import app from './app';
 
-const port = process.env.APP_PORT;
+const port = process.env.PORT || 3000;
 
-app.listen(port);
+app.listen(port, () => {
+  console.log();
+  console.log(`Port ${port}`);
+  console.log(`CTRL + Clique em http://localhost:${port}`);
+});
